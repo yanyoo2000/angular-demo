f29b1f55c48eac82451d0754a832db2fde33afce
yanyoo2000 <1784135426@qq.com>
2022-10-27 11:26:15 +0800

    把测试代码改成ts

--- src/app/js-test/demo.js
+++
@@ -1,7 +0,0 @@
-// create an ArrayBuffer with a size in bytes
-const buffer = new ArrayBuffer(100000000);
-
-let an = performance.now()
-
-let buffer2 = buffer.slice(0)
-console.log(performance.now() - an);
